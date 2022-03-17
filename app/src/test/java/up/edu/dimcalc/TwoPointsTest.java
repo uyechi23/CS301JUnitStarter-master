1ab371a16c62e9d1529228720c3a3cef6a062a02
Halcyon <uyechi23@up.edu>
2022-03-16 18:55:20 -0700

    Initial commit

---
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -0,0 +1,36 @@
+package up.edu.dimcalc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwoPointsTest {
+
+    @Test
+    public void getPoint() {
+    }
+
+    @Test
+    public void setPoint() {
+    }
+
+    @Test
+    public void randomValue() {
+    }
+
+    @Test
+    public void setOrigin() {
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void distance() {
+    }
+
+    @Test
+    public void slope() {
+    }
+}
